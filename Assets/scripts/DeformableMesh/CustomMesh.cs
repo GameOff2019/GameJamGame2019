b3c7589b1f27a067dc52fd9bba0bee9e991931c3
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-11-08 21:27:05 -0500

    procedual mesh

---
+++ Assets/scripts/DeformableMesh/CustomMesh.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class CustomMesh : MonoBehaviour {
+
+    public int x_size, y_size;
+    public float x_cell_size, y_cell_size;
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    void Start() {
+        GenerateMesh();
+
+    }
+
+    void Update() {
+        
+    }
+
+    private void GenerateMesh() {
+        //Generate mesh array
+        vertices = new Vector3[(x_size + 1) * (y_size + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        for (int i = 0, y = 0; y <= y_size; y++) {
+            for (int x = 0; x <= x_size; x++, i++) {
+                vertices[i] = new Vector3(x * x_cell_size, y * y_cell_size, 0);
+                uv[i] = new Vector2(x * x_cell_size / x_size, y * y_cell_size / y_size);
+            }
+        }
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = vertices;
+
+        
+        //Generate triangles
+        int[] triangles = new int[x_size * y_size * 6];
+        for (int y = 0, tri = 0, count = 0; y < y_size; y++, count++) { 
+
+            for (int x = 0; x < x_size; x++, tri += 6, count++) {
+                //triangle1
+                triangles[tri] = count;
+                triangles[tri + 1] = x_size + count + 1;
+                triangles[tri + 2] = count + 1;
+
+                //triangle2
+                triangles[tri + 3] = count + 1;
+                triangles[tri + 4] = x_size + count + 1;
+                triangles[tri + 5] = x_size + count + 2;
+            }
+        }
+
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        mesh.uv = uv;
+        
+
+
+    }
+
+    private void OnDrawGizmos() {
+        Vector3 t_pos = transform.position;
+        Gizmos.color = Color.blue;
+        foreach (Vector3 pos in vertices) {
+            if (pos == null) { continue; }
+            Gizmos.DrawSphere(new Vector3(t_pos.x+pos.x,t_pos.y+pos.y,t_pos.z+pos.z), 0.05f);
+        }
+    }
+}
